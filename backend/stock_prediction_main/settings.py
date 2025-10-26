d55f518a31a55e1da623ea7405c938c6d8a8a4cf
sibaram <sm60786@gmail.com>
2025-10-26 19:53:20 +0530

    Added VITE ROOT URL in .env.local

--- backend/stock_prediction_main/settings.py
+++ backend/stock_prediction_main/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,4 +148,4 @@ SIMPLE_JWT = {
 
 # Media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
